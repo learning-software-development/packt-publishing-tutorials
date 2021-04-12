2c544f11d5d5c8d6856e34a54a91af85c9c45288
Quintin Henn <quintin.henn@gmail.com>
2021-04-12 07:13:01 +0200

    Java Fundamentals exercise 3

---
+++ java-fundamentals/exercise-3-non-ascii-characters/compile.bat
@@ -0,0 +1,5 @@
+IF NOT EXIST build\classes (
+  mkdir build\classes
+)
+
+javac @options @classes
---
+++ java-fundamentals/exercise-3-non-ascii-characters/src/tutorial/HelloNonASCIIWorld.java
@@ -0,0 +1,9 @@
+package tutorial;
+
+public class HelloNonASCIIWorld {
+  public static void main(String[] args) {
+    System.out.println("Non-ASCII characters: ☺");
+    System.out.println("∀x ∈ ℝ: ⌈x⌉ = −⌊−x⌋");
+    System.out.println("π ≅ " + 3.1415926535); // + is used to concatenate
+  }
+}
