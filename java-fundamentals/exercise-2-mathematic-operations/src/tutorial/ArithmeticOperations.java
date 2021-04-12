5997eb9268c8b19aeba02d89ef639193fc8ee46e
Quintin Henn <quintin.henn@gmail.com>
2021-04-12 07:12:50 +0200

    Java Fundamentals exercise 2

---
+++ java-fundamentals/exercise-2-mathematic-operations/src/tutorial/ArithmeticOperations.java
@@ -0,0 +1,10 @@
+package tutorial;
+
+public class ArithmeticOperations {
+  public static void main(String[] args) {
+    System.out.println(4 + 5);
+    System.out.println(4 * 5);
+    System.out.println(4 / 5);
+    System.out.println(9 / 2);
+  }
+}
